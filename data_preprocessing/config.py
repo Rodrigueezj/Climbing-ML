c5bebf976bbc26c821a7cfbc23525b2aeceb26ba
Juan D. Bello <jdbelloro@unal.edu.co>
2025-07-03 09:27:01 -0500

    adjusted coordinates

--- data_preprocessing/config.py
+++ data_preprocessing/config.py
@@ -1,14 +1,16 @@
 # config.py
 
 # Coordinates (Y1, Y2, X1, X2)
-HEADER_COORDS = (175, 270, 500, 1100)
-BOARD_COORDS = (350, 2300, 100, 1500)
+HEADER_COORDS = (175, 300, 500, 1100)
+BOARD_COORDS = (460, 2260, 290, 1390)
+#BOARD_COORDS = (450, 2270, 537, 637)
 
 # Base directory
 BASE_IMAGE_DIR = "data"
 
 # Subfolders
 RAW_DIR     = f"{BASE_IMAGE_DIR}/raw"
+RAW_TEST_DIR = f"{BASE_IMAGE_DIR}/raw_test"
 
 # Output directories
 HEADERS_DIR = f"{BASE_IMAGE_DIR}/headers"
